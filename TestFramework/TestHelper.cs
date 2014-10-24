d484ad3699c62187192a0559aec777612fb4fe9c
Justin Welsch <j.welsch@gmail.com>
2014-10-24 13:51:06 -0400

    Checking results and expected values for null.

--- TestFramework/TestHelper.cs
+++ TestFramework/TestHelper.cs
@@ -8,6 +8,11 @@ namespace TestFramework
 
       public static void ExpectedException<T>( Action method, int stackUnwind = 1 )
       {
+         if ( method == null )
+         {
+            throw new ArgumentNullException( "method", "The method to call cannot be null." );
+         }
+
          var caught = false;
          var expectedType = typeof( T );
 
@@ -31,9 +36,33 @@ namespace TestFramework
 
       public static void Expected<T>( T expected, ActionT<T> method, int stackUnwind = 1 ) //where T : IEquatable<T>
       {
+         if ( method == null )
+         {
+            throw new ArgumentNullException( "method", "The method to call cannot be null." );
+         }
+
+         var throwException = false;
          var result = method();
 
-         if ( !result.Equals( expected ) )
+         if ( ( result == null ) && ( expected != null ) )
+         {
+            throwException = true;
+         }
+
+         if ( ( result != null ) && ( expected == null ) )
+         {
+            throwException = true;
+         }
+
+         if ( ( result != null ) && ( expected != null ) )
+         {
+            if ( !result.Equals( expected ) )
+            {
+               throwException = true;
+            }
+         }
+
+         if ( throwException )
          {
             throw new TestException( String.Format( "Expected value: {0}, but got value: {1}.", expected, result ), 1 + stackUnwind );
          }
