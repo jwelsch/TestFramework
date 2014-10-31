bed8838f9b561c82f271088594f8a8391d98d5da
Justin Welsch <j.welsch@gmail.com>
2014-10-30 20:05:01 -0400

    Adding missing TestResult.cs file

---
+++ TestFramework/TestResult.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace TestFramework
+{
+   public enum TestResultDisposition
+   {
+      Success,
+      Fail
+   }
+
+   public class TestResult
+   {
+      public string Name
+      {
+         get;
+         private set;
+      }
+
+      public TestResultDisposition Disposition
+      {
+         get;
+         private set;
+      }
+
+      public bool IsSuccessful
+      {
+         get { return this.Disposition == TestResultDisposition.Success; }
+      }
+
+      public Exception Exception
+      {
+         get;
+         private set;
+      }
+
+      public object FailContext
+      {
+         get;
+         private set;
+      }
+
+      private TestResult( string name, TestResultDisposition disposition, Exception exception, object failContext )
+      {
+         this.Name = name;
+         this.Disposition = disposition;
+         this.Exception = exception;
+         this.FailContext = failContext;
+      }
+
+      public static TestResult Suceeded( string name )
+      {
+         return new TestResult( name, TestResultDisposition.Success, null, null );
+      }
+
+      public static TestResult Failed( string name, Exception exception )
+      {
+         return new TestResult( name, TestResultDisposition.Fail, exception, null );
+      }
+
+      public static TestResult Failed( string name, Exception exception, object failContext )
+      {
+         return new TestResult( name, TestResultDisposition.Fail, exception, failContext );
+      }
+   }
+}
